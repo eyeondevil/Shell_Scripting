0af07cdb990abd15a4c5477046193695f9703baa
DJ <devops@example.com>
2023-10-04 17:58:38 +0530

    Updated the script to automatically check each minute

--- docker.sh
+++ docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo
-echo "============ Please check the Docker service status and provide the Output ============"
+echo "=========== Check the Docker service Status ==========="
 status="`systemctl status docker | awk 'NR==3 {print}' |cut -d ':' -f 2 | cut -d '(' -f 1`"
 echo $status
 if [ $status = "active" ];
 then
-		echo "The service is up and running fine...."
+                echo "The service is up and running fine...."
 else
-		echo "The service is not running or is dead"
-		sudo systemctl restart docker
+                echo "The service is not running or is dead"
+                sudo systemctl restart docker
+		sudo systemctl status docker
 fi
