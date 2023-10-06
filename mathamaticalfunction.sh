da170d81b3c1b11057b62586923ed1ef0031e784
DJ <devops@example.com>
2023-10-06 10:10:57 +0530

    This script will provide us with the output after enterning any two numbers from the user, but using function calling.

---
+++ mathamaticalfunction.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Author: DJ
+# This script will be be a calculator script.
+
+clear
+echo "--------------------------------"
+echo "------- welcome to calculator ----"
+echo "--------------------------------"
+read_input()
+{
+	read -p "Enter first number: " num1
+	read -p "Enter second number: " num2
+}
+echo -e "[a]Addition\n[b]Subtraction\n[c]Multiplication\n[d]Division\n"
+read -p "Enter your choice: " choice
+case $choice in
+   [aA])
+        read_input
+        result=$((num1+num2))
+        echo "The result for your choice is: $result"
+        ;;
+   [bB])
+        read_input
+        result=$((num1-num2))
+        echo "The result for your choice is: $result"
+        ;;
+   [cC])
+        read_input
+        result=$((num1*num2))
+        echo "The result for your choice is: $result"
+        ;;
+   [dD])
+        read_input
+        result=$((num1/num2))
+        echo "The result for your choice is: $result"
+        ;;
+   *)
+       echo "Wrong choice"
+       ;;
+esac
